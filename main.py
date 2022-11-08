a5bb687fdb8306cdf71d36735f1672cb2f244b5e
Maxim Smajda <smajdamaxim@gmail.com>
2022-11-08 13:23:11 +0100

    Added comments and cleaned code a bit.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# printing puzzle board
 def print_board(board_to_print, size):
     for y in range(size):
         for x in range(size):
@@ -5,18 +6,15 @@ def print_board(board_to_print, size):
         print()
 
 
-def print_clues(board_to_print, size):
-    for y in range(4):
-        for x in range(size):
-            print(board_to_print[y][x], end=' ')
-        print()
-
-
+# initialize empty board full o zeroes of given size
 def init_board(board_size):
     new_board = [[0 for i in range(board_size)] for j in range(board_size)]
     return new_board
 
 
+# ---------------------------------------------
+# ------------- prefill functions -------------
+# ---------------------------------------------
 def fill_from_top(board, clue, column, size):
     for y in range(clue):
         board[y][column] = (size + 1) - clue + y
@@ -32,10 +30,106 @@ def fill_from_left(board, clue, row, size):
         board[row][x] = (size + 1) - clue + x
 
 
+def fill_from_right(board, clue, row, size):
+    for x in range(clue):
+        board[row][size - 1 - x] = (size + 1) - clue + x
+
+
+# quickly fill boxes that are under 1 or size clue
+def prefill_board(board, clues, size):
+    for y in range(4):  # iterating trough clues
+        for x in range(size):
+            if (clues[y][x] == size) or (clues[y][x] == 1):
+                if y == 0:  # top clues
+                    fill_from_top(board, clues[y][x], x, size)
+                elif y == 1:  # bottom clues
+                    fill_from_bottom(board, clues[y][x], x, size)
+                elif y == 2:  # left clues
+                    fill_from_left(board, clues[y][x], x, size)
+                elif y == 3:  # right clues
+                    fill_from_right(board, clues[y][x], x, size)
+
+# ---------------------------------------------
+
+
+# ----------------------------------------------
+# ------------- checking functions -------------
+# ----------------------------------------------
+
+# next four functions count number of visible boxes from each side, which we will use in next checking functions
+# count visible boxes from top view
+def count_visible_top(board, column):
+    visible_count = 0
+    visible_max = 0
+    for y in range(len(board)):
+        if board[y][column] > visible_max:
+            visible_max = board[y][column]
+            visible_count += 1
+    return visible_count
+
+
+# count visible boxes from bottom view
+def count_visible_bottom(board, column):
+    visible_count = 0
+    visible_max = 0
+    for y in range(len(board) - 1, -1, -1):
+        if board[y][column] > visible_max:
+            visible_max = board[y][column]
+            visible_count += 1
+    return visible_count
+
+
+# count visible boxes from left view
+def count_visible_left(board, row):
+    visible_count = 0
+    visible_max = 0
+    for x in range(len(board)):
+        if board[row][x] > visible_max:
+            visible_max = board[row][x]
+            visible_count += 1
+    return visible_count
+
+
+# count visible boxes from right view
+def count_visible_right(board, row):
+    visible_count = 0
+    visible_max = 0
+    for x in range(len(board) - 1, -1, -1):
+        if board[row][x] > visible_max:
+            visible_max = board[row][x]
+            visible_count += 1
+    return visible_count
+
+
+# check if row is full of non-zero numbers
+def row_is_full(board, row):
+    for x in range(len(board)):
+        if board[row][x] == 0:
+            return False
+    return True
+
+
+# check if column is full of non-zero numbers
+def column_is_full(board, column):
+    for y in range(len(board)):
+        if board[y][column] == 0:
+            return False
+    return True
+
+
+# next two functions work only if row or column is full
+# checking if left and right clues are satisfied, number of visible boxes has to be equal to given clue
+def check_row_clues(board, clues, row):
+    return (count_visible_left(board, row) == clues[2][row]) and (count_visible_right(board, row) == clues[3][row])
+
+
+# checking if top and bottom clues are satisfied, number of visible boxes has to be equal to given clue
 def check_column_clues(board, clues, column):
     return (count_visible_top(board, column) == clues[0][column]) and (count_visible_bottom(board, column) == clues[1][column])
 
 
+# we are filling board from top left corner, so we can check in process of filling top and left clues
+# if there are more visible boxes than it is allowed, functions return False, True otherwise
 def check_top_clue(board, clues, column):
     return count_visible_top(board, column) > clues[0][column]
 
@@ -44,6 +138,24 @@ def check_left_clue(board, clues, row):
     return count_visible_left(board, row) > clues[2][row]
 
 
+# checking if number that we are trying to put in box is already in row
+def nb_in_row(board, row, column, nb):
+    for x in range(len(board)):
+        if board[row][x] == nb and x != column:
+            return True
+    return False
+
+
+# checking if number that we are trying to put in box is already in column
+def nb_in_col(board, row, column, nb):
+    for y in range(len(board)):
+        if board[y][column] == nb and y != row:
+            return True
+    return False
+
+
+# validation for given number in box with all checking functions above
+# if all conditions are satisfied, given number is possible on the position on the board
 def validate(board, clues, row, column, nb):
     if nb_in_row(board, row, column, nb):
         return False
@@ -61,42 +173,8 @@ def validate(board, clues, row, column, nb):
             return False
     return True
 
-def solve(board, clues, size):
-    if find_zero(board, size) is None:
-        return board
-    row, column = find_zero(board, size)
-    for nb in range(1, size + 1):
-        board[row][column] = nb
-        # print_board(board, size)
-        # print()
-        if validate(board, clues, row, column, nb):
-            if solve(board, clues, size) is not None:
-                return board
-            board[row][column] = 0
-        else:
-            board[row][column] = 0
-    return None
 
-
-def fill_from_right(board, clue, row, size):
-    for x in range(clue):
-        board[row][size - 1 - x] = (size + 1) - clue + x
-
-
-def prefill_board(board, clues, size):
-    for y in range(4):
-        for x in range(size):
-            if (clues[y][x] == size) or (clues[y][x] == 1):
-                if y == 0:
-                    fill_from_top(board, clues[y][x], x, size)
-                elif y == 1:
-                    fill_from_bottom(board, clues[y][x], x, size)
-                elif y == 2:
-                    fill_from_left(board, clues[y][x], x, size)
-                elif y == 3:
-                    fill_from_right(board, clues[y][x], x, size)
-
-
+# find first zero box in board, searching from top left corner
 def find_zero(board, size):
     for y in range(size):
         for x in range(size):
@@ -105,78 +183,23 @@ def find_zero(board, size):
     return None
 
 
-def nb_in_row(board, row, column, nb):
-    for x in range(len(board)):
-        if board[row][x] == nb and x != column:
-            return True
-    return False
+# solving function goes through all zero boxes and tries all possible numbers
+def solve(board, clues, size):
+    if find_zero(board, size) is None:  # find next zero box
+        return board  # if all boxes are filled with non-zero numbers, puzzle is solved
+    row, column = find_zero(board, size)
+    for nb in range(1, size + 1):
+        board[row][column] = nb  # set number on position we are trying to fill
+        if validate(board, clues, row, column, nb):  # if number satisfies all conditions, go to next zero box
+            if solve(board, clues, size) is not None:
+                return board
+            board[row][column] = 0
+        else:  # if number is not possible on position try another
+            board[row][column] = 0
+    return None  # if there is no possible combination, that means clues are incorrect and return None
 
 
-def nb_in_col(board, row, column, nb):
-    for y in range(len(board)):
-        if board[y][column] == nb and y != row:
-            return True
-    return False
-
-
-def count_visible_top(board, column):
-    visible_count = 0
-    visible_max = 0
-    for y in range(len(board)):
-        if board[y][column] > visible_max:
-            visible_max = board[y][column]
-            visible_count += 1
-    return visible_count
-
-
-def count_visible_bottom(board, column):
-    visible_count = 0
-    visible_max = 0
-    for y in range(len(board) - 1, -1, -1):
-        if board[y][column] > visible_max:
-            visible_max = board[y][column]
-            visible_count += 1
-    return visible_count
-
-
-def count_visible_left(board, row):
-    visible_count = 0
-    visible_max = 0
-    for x in range(len(board)):
-        if board[row][x] > visible_max:
-            visible_max = board[row][x]
-            visible_count += 1
-    return visible_count
-
-
-def count_visible_right(board, row):
-    visible_count = 0
-    visible_max = 0
-    for x in range(len(board) - 1, -1, -1):
-        if board[row][x] > visible_max:
-            visible_max = board[row][x]
-            visible_count += 1
-    return visible_count
-
-
-def row_is_full(board, row):
-    for x in range(len(board)):
-        if board[row][x] == 0:
-            return False
-    return True
-
-
-def column_is_full(board, column):
-    for y in range(len(board)):
-        if board[y][column] == 0:
-            return False
-    return True
-
-
-def check_row_clues(board, clues, row):
-    return (count_visible_left(board, row) == clues[2][row]) and (count_visible_right(board, row) == clues[3][row])
-
-
+# user input of size of the puzzle and clues
 def input_clues():
     while True:
         size = input("Enter size of board: ")
@@ -184,11 +207,11 @@ def input_clues():
             size = int(size)
             break
         else:
-            print("Invalid size! Please enter number!")
+            print("Invalid character! Please enter number!")
 
     print(f"Clues for {size}x{size} board")
-    clues = [[0 for i in range(size)] for j in range(4)]
-    for y in range(4):
+    clues = [[0 for i in range(size)] for j in range(4)]  # initialize array for clues
+    for y in range(4):  # fill array of clues
         if y == 0:
             print("Enter top clues:")
         elif y == 1:
@@ -208,42 +231,19 @@ def input_clues():
                     else:
                         print(f"Invalid input! Number is not in range {1} to {size}")
                 else:
-                    print("Please enter number!")
+                    print("Invalid character! Please enter number!")
     return clues
 
 
 if __name__ == '__main__':
-    # 9x9
-    # clues = [[2, 4, 4, 3, 1, 2, 4, 4, 3], [4, 4, 3, 1, 3, 4, 2, 2, 4], [2, 1, 2, 4, 2, 3, 4, 5, 3], [2, 4, 2, 1, 5, 3, 2, 3, 3]]
-    # board[0][2] = 3
-    # board[0][5] = 4
-    # board[0][7] = 2
-    # board[1][2] = 7
-    # board[1][3] = 8
-    # board[2][2] = 4
-    # board[2][3] = 6
-    # board[2][4] = 3
-    # board[2][6] = 5
-    # board[3][3] = 7
-    # board[3][4] = 2
-    # board[3][7] = 4
-    # board[4][0] = 6
-    # board[4][5] = 8
-    # board[5][0] = 2
-    # board[5][5] = 5
-    # board[5][6] = 3
-    # board[6][5] = 2
-    # board[6][8] = 1
-    # # board[7][1] = 3
-    # # board[7][2] = 6
-
     clues = input_clues()
     size = len(clues[0])
     board = init_board(size)
     prefill_board(board, clues, size)
+
     solution = solve(board, clues, size)
     if solution is None:
-        print("Puzzle not possible with given clues:(")
+        print("Puzzle not possible with given clues :(")
     else:
         print("Solution")
         print_board(solution, size)
